63ed70964497400551e9e5f00c32af39dc973b56
Mohamed Dalmar <163414289+Mohamed-Designer-Developer@users.noreply.github.com>
2024-07-16 00:30:00 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+// DOM selecting 
+let time = document.getElementById('time');
+let date = document.getElementById('date');
+
+//Get current Working Clock
+setInterval(() => {
+    today() //this function get the time, month and year
+}, 1000)
+
+
+// Get current date and time
+function today() {
+    const today = new Date();
+    
+    const CurrentTime = `${today.getHours() <= 9 ? '0' + today.getHours() : today.getHours()} : ${today.getMinutes() <= 9 ? '0' + today.getMinutes() : today.getMinutes() } : ${today.getSeconds() <= 9 ? '0' + today.getSeconds() : today.getSeconds() }`;
+
+    time.innerHTML = CurrentTime;
+
+    const monthNames =['Jan' , 'Feb', 'March', 'April' , 'May' , 'Jun' , 'July','Augt' , 'Sept' , 'Oct' , 'Nov' , 'Dec'];
+
+    const dayNames =['Sat' , 'Sun', 'Mon', 'Tues' , 'Wed' , 'Thur', 'Fri'];
+
+    const currentDate = `${dayNames[today.getDay() + 1]} , ${monthNames[today.getMonth()]} , ${today.getFullYear()}`;
+
+    date.innerHTML = currentDate;
+
+}
+
